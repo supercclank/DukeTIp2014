54b314053792bd7c6a50da9e78ee43d48e66c6b9
supercclank <supercclank@gmail.com>
2014-06-26 13:45:29 -0500

    Edit typos...
    edited for typos and such…

--- Term1/login.php
+++ Term1/login.php
@@ -7,7 +7,7 @@
 //print the connection info	
 	echo $mysqli->host_info . "\n";
 
-	$mysqli = new mysqli("127.0.0.1", "root", "kspnw", "test", 3306); //make the connection to the server
+	$mysqli = new mysqli("127.0.0.1", "root", "", "OUR DATABASE NAME GOES HERE", 3306); //make the connection to the server
 //print the error messages
 	if ($mysqli->connect_errno) {
 	    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
